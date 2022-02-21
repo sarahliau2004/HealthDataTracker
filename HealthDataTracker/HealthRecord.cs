8269847bec2e2c2dd17eb5a0484bb2d50189407d
sarahliau2004 <97188161+sarahliau2004@users.noreply.github.com>
2022-02-20 23:46:49 -0600

    Update HealthRecord.cs

--- HealthDataTracker/HealthRecord.cs
+++ HealthDataTracker/HealthRecord.cs
@@ -8,9 +8,13 @@ namespace HealthDataTracker
 {
     internal class HealthRecord
     {
+        //this is the date field, represented in the mm/dd/yyyy format
         public String date { get; set; }
+        //this is the systolic blood pressure field, represented as an integer
         public int systolicBloodPressure { get; set; }
+        //this is the diastolic blood pressure field, represented as an integer
         public int diastolicBloodPressure { get; set; }
+        //this is the blood sugar field, represented as an integer
         public int bloodSugar { get; set; }
     }
 }
